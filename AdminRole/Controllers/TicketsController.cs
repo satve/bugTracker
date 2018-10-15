a0067af871a91df88c20196c62b42c10348b38af
Satveer Dhillon <Satveerdhillon01@gmail.com>
2018-10-15 08:31:43 -0500

    final commit

--- AdminRole/Controllers/TicketsController.cs
+++ AdminRole/Controllers/TicketsController.cs
@@ -6,14 +6,22 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using AdminRole.helper;
 using AdminRole.Models;
 using Microsoft.AspNet.Identity;
 
 namespace AdminRole.Controllers
 {
+    [Authorize]
     public class TicketsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationDbContext db { get; set; }
+        private UserRoleHelper UserRoleHelper { get; set; }
+        public TicketsController()
+        {
+            db = new ApplicationDbContext();
+            UserRoleHelper = new UserRoleHelper();
+        }
 
         // GET: Tickets
         public ActionResult Index()
@@ -22,6 +30,31 @@ namespace AdminRole.Controllers
             return View(ticket.ToList());
         }
 
+        public ActionResult UserTickets()
+        {
+            string userID = User.Identity.GetUserId();
+            if (User.IsInRole("Submitter"))
+            {
+                var tickets = db.Ticket.Where(t => t.CreatorId == userID).Include(t => t.Creator).Include(t => t.Assignee).Include(t => t.Bug);
+                return View("Index", tickets.ToList());
+            }
+            if (User.IsInRole("Developer"))
+            {
+                var tickets = db.Ticket.Where(t => t.AssigneeId == userID).Include(t => t.Creator).Include(t => t.Assignee).Include(t => t.Bug);
+                return View("Index", tickets.ToList());
+            }
+            return View("Index");
+        }
+        // Project Manger and Developer Tickets
+        [Authorize(Roles = "Project Manager,Developer")]
+        public ActionResult ProjectManagerOrDeveloperTickets()
+        {
+            string userId = User.Identity.GetUserId();
+            var ProjectMangerOrDeveloperId = db.Users.Where(p => p.Id == userId).FirstOrDefault();
+            var ProjectId = ProjectMangerOrDeveloperId.Bugs.Select(p => p.Id).FirstOrDefault();
+            var tickets = db.Ticket.Where(p => p.Id == ProjectId).ToList();
+            return View("Index", tickets);
+        }
         // GET: Tickets/Details/5
         public ActionResult Details(int? id)
         {
@@ -38,12 +71,12 @@ namespace AdminRole.Controllers
         }
 
         // GET: Tickets/Create
+        [Authorize(Roles = "Submitter")]
         public ActionResult Create()
         {
             ViewBag.AssigneeId = new SelectList(db.Users, "Id", "FirstName");
             ViewBag.BugId = new SelectList(db.Bugs, "Id", "Name");
             ViewBag.CreatorId = new SelectList(db.Users, "Id", "FirstName");
-            ViewBag.TicketStatusId = new SelectList(db.TicketStatus, "Id", "Name");
             ViewBag.TicketPriorityId = new SelectList(db.TicketPriority, "Id", "Name");
             ViewBag.TicketTypeId = new SelectList(db.TicketType, "Id", "Name");
             return View();
@@ -54,6 +87,7 @@ namespace AdminRole.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Submitter")]
         public ActionResult Create([Bind(Include = "Id,Name,Description,TicketTypeId,TicketPriorityId,BugId")] Ticket ticket)
         {
             if (ModelState.IsValid)
@@ -97,11 +131,15 @@ namespace AdminRole.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,Created,Updated,TicketTypeId,TicketPriorityId,TicketStatusId,CreatorId,AssigneeId,BugId")] Ticket ticket)
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,TicketTypeId,TicketPriorityId,TicketStatusId,CreatorId,AssigneeId,BugId")] Ticket ticket)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(ticket).State = EntityState.Modified;
+                // db.Entry(ticket).State = EntityState.Modified;
+                var DbTic = db.Ticket.FirstOrDefault(p => p.Id == ticket.Id);
+                DbTic.Name = ticket.Name;
+                DbTic.Description = ticket.Description;
+                DbTic.Updated = DateTime.Now;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
